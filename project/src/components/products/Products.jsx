7d2f657535d04b17dce17ed261a9d8e8e4a1b456
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-03-31 01:42:55 +0530

    update

--- project/src/components/products/Products.jsx
+++ project/src/components/products/Products.jsx
@@ -8,6 +8,10 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
+import Checkbox from '@mui/material/Checkbox';
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 // import styled from "styled-components";
 export const Products = () => {
 
@@ -37,35 +41,39 @@ export const Products = () => {
                         <FormControlLabel value="Fastest Shipping" control={<Radio />} label="Fastest Shipping" />
                         <FormControlLabel value="Newest" control={<Radio />} label="Newest" />
                     </RadioGroup>
-                    <br/>
-                    <FormLabel id="demo-radio-buttons-group-label">Sort by</FormLabel>
+                    </FormControl>
+                  <hr />
+                  <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">Discount</FormLabel>
                     <RadioGroup
                         aria-labelledby="demo-radio-buttons-group-label"
                         defaultValue="female"
                         name="radio-buttons-group"
                     >
-                        <FormControlLabel value="Relevance" control={<Radio />} label="10% and Above" />
-                        <FormControlLabel value="Highest Priced First" control={<Radio />} label="20% and Above" />
-                        <FormControlLabel value="Lowest Priced First" control={<Radio />} label="30% and Above" />
-                        <FormControlLabel value="Fastest Shipping" control={<Radio />} label="40% and Above" />
+                        <FormControlLabel value="10% and Above" control={<Radio />} label="10% and Above" />
+                        <FormControlLabel value="20% and Above" control={<Radio />} label="20% and Above" />
+                        <FormControlLabel value="30% and Above" control={<Radio />} label="30% and Above" />
+                        <FormControlLabel value="40% and Above" control={<Radio />} label="40% and Above" />
                     </RadioGroup>
                 </FormControl>
+                <hr />
             </div>
             <div className="right">
                 {data.map((e) => {
                     return (
                         <div key={e._id} className="chair">
                             <Link to={`/armschair/:${e._id}`}>
-                                <div key={e._id}>
+                                <div key={e._id} className="overlay">
+                                    <div>
                                     <img src={e.image} alt="chair" />
+                                    <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                                    </div>
                                     <h3>{e.name}</h3>
                                 </div>
                             </Link>
                         </div>
                     )
                 })}
-                {/* </div> */}
-
             </div>
         </div>
     )
