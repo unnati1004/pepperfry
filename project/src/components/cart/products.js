9353ff04b158c4f6105058d88389962b2bb2d550
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-02 12:18:20 +0530

    cart

---
+++ project/src/components/cart/products.js
@@ -0,0 +1,95 @@
+export const products = [
+    {
+        id:1,
+        title:"Designer Cantilever Chair in Black Colour",
+        creator:"Akdio System",
+        price:"10882",
+        img:"https://ii2.pepperfry.com/media/catalog/product/d/e/494x544/designer-cantilever-chair-in-black-colour-by-adiko-systems-designer-cantilever-chair-in-black-colour-i8dzum.jpg",
+        quantity:1
+    },
+
+    {
+        id:2,
+        title:"Elegant Cantilever Chair in Black Colour",
+        creator:"Akdio System",
+        price:'8420',
+        img:"https://ii3.pepperfry.com/media/catalog/product/e/l/494x544/elegant-cantilever-chair-in-black-colour-by-adiko-systems-elegant-cantilever-chair-in-black-colour-b-w5x33t.jpg",
+        quantity:1
+    },
+
+    {
+        id:3,
+        title:"Erica Cantilever Chair in Black & White Colour",
+        creator:"Creative Seating Sytem",
+        price:"6998",
+        img:"https://ii1.pepperfry.com/media/catalog/product/e/r/494x544/erica-cantilever-chair-in-black---white-colour-by-creative-seating-system-erica-cantilever-chair-in--of0o4c.jpg",
+        quantity:1,
+
+    },
+    {
+        id:4,
+        title:"Modista Cantilever Chair in Dark Brown Colour",
+        creator:"DZYN Furnitures",
+        price:"7990",
+        img:"https://ii2.pepperfry.com/media/catalog/product/m/o/494x544/modista-cantilever-chair-in-dark-brown-colour-by-dzyn-furnitures-modista-cantilever-chair-in-dark-br-dhiaqa.jpg",
+        quantity:1,
+
+    },
+    {
+        id:5,
+        title:"Envidiable Cantilever Chair in Dark Brown Colour",
+        creator:"DZYN Furnitures",
+        price:"6990",
+        img:"https://ii3.pepperfry.com/media/catalog/product/a/g/494x544/agronomo-cantilever-chair-in-black-colour-by-dzyn-furnitures-agronomo-cantilever-chair-in-black-colo-cdtmzy.jpg",
+        quantity:1,
+
+    },
+    {
+        id:6,
+        title:"Elvina Cantilever Chair in Black & White Colour",
+        creator:"DZYN Furnitures",
+        price:"6998",
+        img:"https://ii1.pepperfry.com/media/catalog/product/b/r/494x544/bravo-guest-chair-in-brown-colour-by-furniease-bravo-guest-chair-in-brown-colour-by-furniease-tahzew.jpg",
+        quantity:1,
+
+    },
+    {
+        id:7,
+        title:"Novelty Cantilever Office Chair in Black Colour",
+        creator:"VOF",
+        price:"7775",
+        img:"https://ii2.pepperfry.com/media/catalog/product/n/o/494x544/novelty-cantilever-office-chair-in-black-colour-by-vof-novelty-cantilever-office-chair-in-black-colo-zbzgue.jpg",
+        quantity:1,
+
+    },
+    {
+        id:8,
+        title:"Agronomo Cantilever Chair in Black Colour",
+        creator:"VOF",
+        price:"5606",
+        img:"https://ii3.pepperfry.com/media/catalog/product/a/g/494x544/agronomo-cantilever-chair-in-black-colour-by-dzyn-furnitures-agronomo-cantilever-chair-in-black-colo-cdtmzy.jpg",
+        quantity:1,
+
+    },
+    {
+        id:9,
+        title:"Bravo Guest Chair in Brown Colour",
+        creator:"VOF",
+        price:"5745",
+        img:"https://ii3.pepperfry.com/media/catalog/product/z/i/494x544/zig-zag-cantilever-chair-in-black-colour-by-luxor-zig-zag-cantilever-chair-in-black-colour-by-luxor-hiytwc.jpg",
+        quantity:1,
+
+    },
+    {
+        id:10,
+        title:"Blezia Cantilever Chair In Black Colour",
+        creator:"Luxor",
+        price:"9999",
+        img:"https://ii3.pepperfry.com/media/catalog/product/b/a/494x544/baxtonn-cantilever-chair-in-white-colour-by-dzyn-furnitures-baxtonn-cantilever-chair-in-white-colour-688a8p.jpg",
+        quantity:1,
+
+    }
+
+
+
+]
