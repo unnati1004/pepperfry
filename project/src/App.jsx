a7f9b031cf8e29309d6cf28405584d03ac925e69
Gulashan Hashami <gulashan959898@gmail.com>
2022-04-02 07:10:26 -0700

    home page

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Routers } from './routers/Routers';
 
 function App() {
   return (
     <div className="App">
+
+    <Routers />
+
     </div>
   );
 }
---
+++ project/src/routers/Routers.jsx
@@ -0,0 +1,14 @@
+import { Home } from "../components/home/Home"
+import { Navbar } from "../components/navbar/Navbar"
+
+
+export const Routers=()=>{
+
+
+    return(
+        <>
+        <Navbar />
+        <Home />
+        </>
+    )
+}
