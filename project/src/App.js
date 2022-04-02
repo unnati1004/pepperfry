9d717f3737a1399871bf917e7dfd7dd37a770055
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-02 12:28:34 +0530

    cart

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import  Billing  from "./components/cart/Billing";
+
+import Cart from './components/cart/Cart';
+
 
 function App() {
   return (
