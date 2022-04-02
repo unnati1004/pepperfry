c5db34dd1c0fa9d456153ebed7b1760b0e5b48c1
Sorav Malviya <soravmalviya15@gmail.com>
2022-04-02 14:24:20 +0530

    payment

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,12 @@
-
-import './App.css';
-import "./components/footer/Footer.css"
-import Footer from './components/footer/Footer.jsx';
+import "./App.css";
+import "./components/footer/Footer.css";
+import Footer from "./components/footer/Footer.jsx";
+import { Payment } from "./components/payment/Payment";
 
 function App() {
   return (
     <div className="App">
+      <Payment />
       <Footer />
     </div>
   );
