3d7f4bd4e4c1a755e1ef4fa50b70f7c37d684dca
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-02 14:14:46 +0530

    cart

--- project/src/routers/Routers.jsx
+++ project/src/routers/Routers.jsx
@@ -1,7 +1,8 @@
 import { Route,Routes } from "react-router-dom";
 import {Products} from "../components/products/Products";
 import {ProductDetail} from "../components/productdetails/Productdetails";
-import Cart from "../"
+import Cart from "../components/cart/Cart"
+import Billing from "../components/cart/Billing"
 export const AllRoutes = ()=>{
     return(
         <>
@@ -9,6 +10,9 @@ export const AllRoutes = ()=>{
           <Routes>
               <Route exact path="/armschair" element={<Products/>}/>
               <Route exact path="/armschair/:id" element={<ProductDetail/>}/>
+              <Route exact path="/cart" element={<Cart/>}/>
+              <Route exact path="/billing" element={<Billing/>}/>
+
           </Routes>
         </>
     )
