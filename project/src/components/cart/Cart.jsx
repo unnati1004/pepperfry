6bc8bc40ccdd8bad8e4619478baf84a022357e91
Aditya7j <89588001+Aditya7j@users.noreply.github.com>
2022-04-02 12:04:26 +0530

    cart

---
+++ project/src/components/cart/Cart.jsx
@@ -0,0 +1,69 @@
+import { createContext, useEffect, useReducer } from "react"
+import "./Cart.css"
+import { products } from "./products"
+import Contextcart from "./Contextcart"
+import { reducer } from "./reducer"
+
+
+
+ export const CartContext = createContext();
+
+ const init_state = {
+     items:products,
+     totalAmount:0,
+     totalItem:0,
+     carttotal:0,
+
+ }
+
+ const Cart=()=>{
+
+    const [state,dispatch] = useReducer(reducer,init_state);
+
+    const removeItems = (id) =>{
+        return dispatch({
+            type:"REMOVE_ITEMS",
+            payload:id,
+
+        })
+
+    }
+
+    const increment = (id)=>{
+        return dispatch({
+            type:"INCREMENT",
+            payload:id,
+        })
+    }
+
+    const decrement = (id)=>{
+        return dispatch({
+            type:"DECREMENT",
+            payload:id,
+        })
+    }
+
+    useEffect(()=>{
+        dispatch({type:"GET_TOTAL"})
+        // console.log("ADitya")
+    },[state.items])
+
+    useEffect(()=>{
+        dispatch({type:"CART_TOTAL"})
+    },[state.items])
+
+    // useEffect(()=>{
+    //     dispatch({type:"TOTAL_AMOUNT"})
+    // },[state.items])
+
+    return (
+        <>
+        <CartContext.Provider value = {{...state,removeItems,increment,decrement}}>
+            <Contextcart/>
+        </CartContext.Provider>
+        </>
+
+    )
+}
+
+export default Cart
---
+++ project/src/components/cart/reducer.js
@@ -0,0 +1,61 @@
+export const reducer = (state,action)=>{
+    if(action.type==="REMOVE_ITEMS"){
+        return {
+            ...state,
+            items:state.items.filter((currele)=>{
+              return currele.id !== action.payload
+            })
+        }
+    }
+
+    if(action.type==="INCREMENT"){
+        const updatedCart = state.items.map((currele)=>{
+            if(currele.id===action.payload){
+                return {...currele,quantity:currele.quantity+1}
+            }
+            return currele;
+        })
+
+        return {...state,items:updatedCart}
+    }
+
+    if(action.type==="DECREMENT"){
+        const updatedCart = state.items.map((currele)=>{
+            if(currele.id===action.payload){
+                return {...currele,quantity:currele.quantity-1}
+            }
+        
+            return currele;
+        }).filter((e)=>{
+            return e.quantity !==0
+
+        })
+        return {...state,items:updatedCart}
+    }
+
+    if(action.type==="GET_TOTAL"){
+        let {totalItem,totalAmount} = state.items.reduce((acc,cv)=>{
+            let {price,quantity} = cv;
+            let updatedTotalamount = price*quantity + 99;
+
+            acc.totalAmount +=updatedTotalamount
+            acc.totalItem +=quantity;
+            return acc;
+        },{totalItem:0,totalAmount:0})
+        return {...state,totalItem,totalAmount}
+    }
+
+    if(action.type === "CART_TOTAL"){
+        let {carttotal} = state.items.reduce((acc,cv)=>{
+            let {price,quantity} =cv;
+            let updatedCartAmount = price*quantity;
+            acc.carttotal +=updatedCartAmount;
+            return acc; 
+        },{carttotal:0})
+        return {...state,carttotal}
+    }
+
+    
+    
+    return state;
+}
