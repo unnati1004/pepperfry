fa2e25cc63ca7afa0b6d3eedc84c805788b0d2df
unnati1004 <95961486+unnati1004@users.noreply.github.com>
2022-04-03 11:51:58 +0530

    update

--- project/src/index.js
+++ project/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./components/redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
